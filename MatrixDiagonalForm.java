6e0106b24c43c6caca8a90c7babfabd418ee1621
Sathya Narayanan C <mail2sathyanc@yahoo.com>
2018-07-12 17:19:55 +0530

    Update MatrixDiagonalForm.java

--- MatrixDiagonalForm.java
+++ MatrixDiagonalForm.java
@@ -1,4 +1,4 @@
-public class MyClass {
+public class MatrixDiagonalForm {
     public static void main(String args[]) {
         int i = 0, j = 0 ; int k;
         int mat[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20}};
